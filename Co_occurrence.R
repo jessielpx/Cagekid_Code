9cbc4709154c29a57c70599ea96913703b0116c0
jessielpx <peixi.liu@mail.mcgill.ca>
2025-03-10 14:29:50 -0400

    add comment

--- Co_occurrence.R
+++ Co_occurrence.R
@@ -1,16 +1,22 @@
-library(maftools)
+# Install and load packages
 install.packages("readxl")
+install.packages("maftools")
 library(readxl)
+library(maftools)
 
-#Input data
+
+# Input data
 maf = read_excel('/Users/jessie/Desktop/Cagekid/Cagekid_All.xlsx', sheet = 'Cagekid_mut') 
 clin = read_excel('/Users/jessie/Desktop/Cagekid/Cagekid_All.xlsx', sheet = 'Cagekid_clin') 
 
-#Filtering
+#Filtering by VHL Status
 clin <- clin[clin$VHL == "1", ]
 
+# Read maf file
 data <- maf[maf$Tumor_Sample_Barcode %in% clin$Tumor_Sample_Barcode, ]
 read <- read.maf(maf = data, clinicalData = clin)
+
+#Generate the plot
 plot <- somaticInteractions(
   maf = read,
   countStats = TRUE,
@@ -18,5 +24,3 @@ plot <- somaticInteractions(
   pvalue = c(0.05, 0.1),
   fontSize = 0.7
 )
-
-
